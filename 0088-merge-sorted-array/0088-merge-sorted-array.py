f766c9bb9887c3539fcba3682ab3ae5f7df77560
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2025-10-04 12:39:00 +0530

    Time: 0 ms (100%), Space: 17.9 MB (26.02%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -1,28 +1,23 @@
-class Solution(object):
-    def merge(self, nums1, m, nums2, n):
-        """
-        :type nums1: List[int]
-        :type m: int
-        :type nums2: List[int]
-        :type n: int
-        :rtype: None Do not return anything, modify nums1 in-place instead.
-        """
-        i=0
-        j=0
-        l=[]
-        while(i<m and j<n):
-            if nums1[i]>nums2[j]:
-                l.append(nums2[j])
-                j+=1
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        if n == 0:
+            return
+        if m == 0:
+            for i in range(n):
+                nums1[i] = nums2[i]
+            return
+        i, j = m - 1 , n - 1
+        k = n + m -1
+        while i >= 0 and j >= 0:
+            if nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
             else:
-                l.append(nums1[i])
-                i+=1
-        while(i<m):
-            l.append(nums1[i])
-            i+=1
-        while(j<n):
-            l.append(nums2[j])
-            j+=1
-        for i in range(len(l)):
-            nums1[i]=l[i]
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1
+        while j >= 0:
+            nums1[k] = nums2[j]
+            j -= 1
+            k -= 1
         
