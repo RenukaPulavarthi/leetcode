275107778cf76602a89eb39e17759e6d2361af93
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2025-10-07 11:24:27 +0530

    Time: 2983 ms (18.17%), Space: 113 MB (5.84%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,23 +1,22 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        dp={}
-        i=0
-        for j in range(0,len(s)):
-            i,k=0,j
-            while i<len(s) and k<len(s):
-                if i==k:
-                    dp[(i,k)]=1
-                elif i+1==k and s[i]==s[k]:
-                    dp[(i,k)]=1
-                elif s[i]==s[k] and (i+1,k-1) in dp:
-                    dp[(i,k)]=1
-                i+=1
-                k+=1
-        ans=""
-        ans_l=0
-        for i in dp:
-            if i[1]-i[0]>ans_l:
-                ans_l=i[1]-i[0]
-                ans=s[i[0]:i[1]+1]
-        return ans;
-                
+        dp = {}
+        n = len(s)
+        ans = [-1,-1]
+        for i in range(n):
+            start = 0
+            end = i
+            while end < n:
+                if start == end and s[start] == s[end]:
+                    dp[(start, end)] = 1
+                    ans[0], ans[1] = start, end
+                elif start + 1 == end and s[start] == s[end]:
+                    dp[(start, end)] = 1
+                    ans[0], ans[1] = start, end
+                elif s[start] == s[end] and (start + 1, end - 1) in dp:
+                    dp[(start, end)] = 1
+                    ans[0], ans[1] = start, end
+                start += 1
+                end += 1
+        return s[ans[0] : ans[1] + 1]
+            
