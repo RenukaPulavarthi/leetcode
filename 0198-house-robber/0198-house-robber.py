da13152e64822eb092175440464f064bae1676fe
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2025-10-11 18:40:50 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,17 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp = {}
+        max_pro = []
         n = len(nums)
-
-        def solve(idx):
-            if idx >= n:
-                return 0
-            if idx in dp:
-                return dp[idx]
-            k = 0
-            for i in range(idx + 2, n):
-                k = max(k, solve(i))
-            dp[idx] = k + nums[idx]
-            return k + nums[idx]
-        
-        return max(solve(0), solve(1))
+        for i in range(n):
+            if i == 0:
+                max_pro.append(nums[0])
+            elif i == 1:
+                max_pro.append(nums[1])
+            else:
+                max_pro.append(max(nums[i] + max_pro[-2], max_pro[-1]))
+        return max_pro[-1]
