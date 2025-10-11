346e257aa1cf57948e466ad125bf3ca049abb88c
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2025-10-11 18:24:08 +0530

    Time: 7 ms (1.57%), Space: 17.8 MB (53.95%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,17 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         dp = {}
+        n = len(nums)
 
         def solve(idx):
-            if idx >= len(nums):
+            if idx >= n:
                 return 0
             if idx in dp:
                 return dp[idx]
-            maxi = 0
-            for i in range(idx,len(nums)):
-                maxi=max(maxi, solve(i+2))
-            dp[idx] = nums[idx] + maxi
-            return nums[idx] + maxi
+            k = 0
+            for i in range(idx + 2, n):
+                k = max(k, solve(i))
+            dp[idx] = k + nums[idx]
+            return k + nums[idx]
         
         return max(solve(0), solve(1))
-        
