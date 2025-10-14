2c38fc7bf13ed54f6b90d69752af8cd3dd78b1ad
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2025-10-14 10:30:32 +0530

    Time: 3 ms (78.85%), Space: 17.8 MB (69.54%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -3,7 +3,13 @@ class Solution:
         nums_map = {1000 :'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
         ans = ""
         for i in nums_map:
-            ans += nums_map[i] * (num // i)
-            num = num % i
+            # print(i, num)
+            if i <= num:
+                z = num // i
+                # print(nums_map[i])
+                ans += (nums_map[i] * z)
+                num = num % i
         return ans
+
+            
 
