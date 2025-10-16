ba5bb80438ffa97f004db10e63adda417f46f364
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2025-10-16 15:23:13 +0530

    Time: 0 ms (100%), Space: 17.9 MB (41.42%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -5,16 +5,13 @@ class Solution:
         """
         n = len(matrix)
         for i in range(n):
-            for j in range(i+1,n):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        
+            for j in range(i+1):
+                matrix[i][j],matrix[j][i] = matrix[j][i], matrix[i][j]
+        print(matrix)
         for i in range(n):
-            low, high = 0 ,n-1
+            low, high = 0, n-1
             while low < high:
                 matrix[i][low], matrix[i][high] = matrix[i][high], matrix[i][low]
-                low+=1
-                high-=1
-        
-        
-
+                low += 1
+                high -= 1
         
