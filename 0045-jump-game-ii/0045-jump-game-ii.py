58e52c0fb1ed6c7f3c5fe67ecc6e354363c05b20
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2025-10-17 15:36:34 +0530

    Time: 7 ms (79.09%), Space: 18.5 MB (91.93%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,13 +1,16 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        cnt = 0
-        left, right = 0, 0
-        while right < len(nums) - 1:
-            maxi = 0
-            for i in range(left, right+1):
-                maxi = max(maxi, i+nums[i])
+        n = len(nums)
+        left, right = 0, 0 
+        ans = 0
+        while right < n - 1:
+            maxi = left
+            for i in range(left, right + 1):
+                maxi = max(maxi, i + nums[i])
             left = right + 1
             right = maxi
-            cnt += 1
-        return cnt
-            
+            ans +=1
+        return ans
+
+
+    
