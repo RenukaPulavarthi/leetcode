4c78539e4683b4f9448fe1cd6087b383fa15c6f3
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2025-10-20 07:25:10 +0530

    Time: 319 ms (37.85%), Space: 18.8 MB (73.52%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -1,21 +1,19 @@
 from collections import deque
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        que = deque()
         wordSet = set(wordList)
-        q = deque()
-        q.append((beginWord,1))
         if beginWord in wordSet:
             wordSet.remove(beginWord)
-        while q:
-            curr, steps = q.popleft()
-            # print(curr)
-            if curr == endWord: return steps
+        que.append((beginWord, 1))
+        while que:
+            curr, steps = que.popleft()
+            if curr == endWord:
+                return steps
             for i in range(len(curr)):
                 for j in 'abcdefghijklmnopqrstuvwxyz':
-                    pre = curr[:i] + j + curr[i+1::]
-                    if pre in wordSet:
-                        wordSet.remove(pre)
-                        q.append((pre,steps + 1))
+                    present = curr[:i] + j + curr[i+1:]
+                    if present in wordSet:
+                        que.append((present, steps + 1))
+                        wordSet.remove(present)
         return 0
-                    
-
