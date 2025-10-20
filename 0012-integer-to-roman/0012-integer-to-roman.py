83ca4f0e5239d0be59286637cd12bf6973c70b5f
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2025-10-20 08:22:18 +0530

    Time: 3 ms (79.06%), Space: 18 MB (5.88%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,15 +1,11 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        nums_map = {1000 :'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
-        ans = ""
-        for i in nums_map:
-            # print(i, num)
+        mapping = { 1000: 'M', 900: 'CM', 500: 'D', 400: 'CD', 100: 'C', 90: 'XC', 50: 'L', 40: 'XL', 10: 'X', 9: 'IX', 5: 'V', 4: 'IV', 1: 'I'}
+        ans = ''
+        for i in mapping:
             if i <= num:
-                z = num // i
-                # print(nums_map[i])
-                ans += (nums_map[i] * z)
+
+                n = num // i
+                ans = ans + (mapping[i] * n)
                 num = num % i
         return ans
-
-            
-
