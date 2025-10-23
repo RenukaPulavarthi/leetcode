138cc8d951ceb21350f2b69782fb17bf50f05cda
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2025-10-24 05:26:47 +0530

    Time: 4 ms (41.21%), Space: 18.9 MB (51.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,10 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
-        nums = [[nums[i], i] for i in range(n)]
-        nums.sort()
-        low, high = 0, n - 1
-        while low < high:
-            if nums[low][0] + nums[high][0] == target:
-                return [nums[low][1], nums[high][1]]
-            elif nums[low][0] + nums[high][0] > target:
-                high -= 1
-            else:
-                low += 1
+        d = {}
+        for i in range(n):
+            d[nums[i]] = i
+        for i in range(n):
+            if target - nums[i] in d and d[target - nums[i]] != i:
+                return [i, d[target - nums[i]]]
         return []
