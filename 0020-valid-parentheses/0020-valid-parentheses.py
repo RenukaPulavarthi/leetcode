c6e03171f5a1a222f1b3331cdd2a1623729bc5fa
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2025-10-29 18:57:59 +0530

    Time: 0 ms (100%), Space: 18.1 MB (15.7%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,11 +4,8 @@ class Solution:
         for i in s:
             if i in '([{':
                 stack.append(i)
+            elif len(stack) > 0 and ((i == ')' and stack[-1] == '(') or (i == ']' and stack[-1] =='[') or (i=='}' and stack[-1] == '{')):
+                stack.pop()
             else:
-                if len(stack) == 0:
-                    return False
-                if (i == ')' and stack[-1] =='(') or (i == ']' and stack[-1] == '[') or (i == '}' and stack[-1] == '{'):
-                    stack.pop()
-                else:
-                    return False
+                return False
         return len(stack) == 0
