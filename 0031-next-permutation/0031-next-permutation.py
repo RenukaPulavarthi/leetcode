055a80b5a7f11bf0ec99bc813aad3507384a3706
RenukaPulavarthi <119840386+RenukaPulavarthi@users.noreply.github.com>
2025-10-31 05:01:12 +0530

    Time: 0 ms (100%), Space: 17.9 MB (14.07%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -3,22 +3,25 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        n = len(nums)
         br_pt = -1
-        for i in range(len(nums)-2, -1, -1):
+        for i in range(n - 2, -1, -1):
             if nums[i] < nums[i+1]:
                 br_pt = i
                 break
         if br_pt != -1:
-            for i in range(len(nums) - 1, br_pt, -1):
+            for i in range(n-1, br_pt, -1):
                 if nums[i] > nums[br_pt]:
                     nums[i], nums[br_pt] = nums[br_pt], nums[i]
                     break
-        i = br_pt + 1
-        j = len(nums) - 1
+        i = br_pt +1
+        j = n-1
         while i < j:
             nums[i], nums[j] = nums[j], nums[i]
             i += 1
             j -= 1
-        return
+        
+        
+            
 
         
